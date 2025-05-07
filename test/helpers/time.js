4a7efcfee95a55f70b45224fc4cefd4c8f14316b
lauren lu <lu.yun2@northeastern.edu>
2025-05-06 20:18:13 -0400

    update gitignore and time.js

--- test/helpers/time.js
+++ test/helpers/time.js
@@ -1,20 +1,35 @@
 async function increase(duration) {
 
-    //first, let's increase time
-    await web3.currentProvider.sendAsync({
-        jsonrpc: "2.0",
-        method: "evm_increaseTime",
-        params: [duration], // there are 86400 seconds in a day
-        id: new Date().getTime()
-    }, () => {});
+    // Check which method is available and use it consistently
+    const sendMethod = web3.currentProvider.send ?
+        web3.currentProvider.send.bind(web3.currentProvider) :
+        web3.currentProvider.sendAsync.bind(web3.currentProvider);
 
-    //next, let's mine a new block
-    web3.currentProvider.send({
-        jsonrpc: '2.0',
-        method: 'evm_mine',
-        params: [],
-        id: new Date().getTime()
-    })
+    // First, let's increase time
+    await new Promise((resolve, reject) => {
+        sendMethod({
+            jsonrpc: "2.0",
+            method: "evm_increaseTime",
+            params: [duration],
+            id: new Date().getTime()
+        }, (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        });
+    });
+
+    // Next, let's mine a new block
+    await new Promise((resolve, reject) => {
+        sendMethod({
+            jsonrpc: '2.0',
+            method: 'evm_mine',
+            params: [],
+            id: new Date().getTime()
+        }, (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        });
+    });
 
 }
 
